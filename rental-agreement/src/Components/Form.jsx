d87d59e3ef33fb9d857893a46b5f846f22ee1896
Alok kothiyal <alokothiyal007@gmail.com>
2020-09-16 18:51:46 +0530

    basic reeact layout

---
+++ rental-agreement/src/Components/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Form extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state ={
+
+        }
+    }
+
+    render{
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
