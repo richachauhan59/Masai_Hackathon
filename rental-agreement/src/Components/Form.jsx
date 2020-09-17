8e7a0b9a171d20ed5c39240b118a2c4e4d6e83d5
Alok kothiyal <alokothiyal007@gmail.com>
2020-09-17 12:00:43 +0530

    added form

--- rental-agreement/src/Components/Form.jsx
+++ rental-agreement/src/Components/Form.jsx
@@ -9,11 +9,35 @@ class Form extends React.Component{
         }
     }
 
-    render{
+    render(){
         return (
-            <form>
+            <div className="container" >
+                <h1>Tenent Registration Form</h1>
+
+            <form className="">
+
+                <div class="form-group">
+                        <label for="exampleInputEmail1">Full Name</label>
+                        <input type="text" class="form-control" id="fullname" aria-describedby="emailHelp"></input>
+                </div>
                 
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"></input>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Contact Info</label>
+                    <input type="Number" class="form-control" id="contact"></input>
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+
             </form>
+            
+            </div>
         )
     }
 }
+export default Form
