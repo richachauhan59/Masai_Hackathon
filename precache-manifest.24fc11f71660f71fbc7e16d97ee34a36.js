b0706c708405021d1e15ca7a03eb9920c33fb9d5
richachauhan59 <richachauhan2019@gmail.com>
2020-11-27 02:27:50 +0530

    Updates

---
+++ precache-manifest.24fc11f71660f71fbc7e16d97ee34a36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b1e57abc6291cb26d97740a4ef5c5c",
+    "url": "/rental-agreement/index.html"
+  },
+  {
+    "revision": "16612e65e6f75a1a2ecb",
+    "url": "/rental-agreement/static/css/main.9b0a200b.chunk.css"
+  },
+  {
+    "revision": "1692957df3de0ef16dfd",
+    "url": "/rental-agreement/static/js/2.d005090b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rental-agreement/static/js/2.d005090b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16612e65e6f75a1a2ecb",
+    "url": "/rental-agreement/static/js/main.37b83762.chunk.js"
+  },
+  {
+    "revision": "fe32262a05708a23c359",
+    "url": "/rental-agreement/static/js/runtime-main.1af144ae.js"
+  },
+  {
+    "revision": "10f0ef19cd4a2c157181601d61a4cb90",
+    "url": "/rental-agreement/static/media/ds.10f0ef19.jpg"
+  },
+  {
+    "revision": "c16ee1f406c4c2eef712476a46f7b844",
+    "url": "/rental-agreement/static/media/img2.c16ee1f4.jpg"
+  }
+]);
